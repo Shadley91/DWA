0f98ba705540ac33f038bcc373dae65ebd3d5a91
Shadley91 <shadley.phil@gmail.com>
2024-02-01 17:23:19 +0200

    initial commit - creates files

---
+++ DWA15/myfirstreactproject/reactinfosite/src/App.jsx
@@ -0,0 +1,15 @@
+import MainContent from "./components/MainContent";
+import Navbar from "./components/NavBar";
+import "./style.css";
+
+function App() {
+  return (
+    <div>
+      <App />
+      <Navbar />;
+      <MainContent />;
+    </div>
+  );
+}
+
+export default App;
